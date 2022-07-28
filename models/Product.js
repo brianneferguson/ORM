25f05b46bd5ce767e816825cb81c6e1e435f1c7d
Brianne <ferguson.brianne@gmail.com>
2022-07-28 17:55:58 -0400

    updates

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { DECIMAL } = require('mysql2/lib/constants/types');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,7 +10,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:true,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    product_name:{
+      type:DataTypes.STRING,
+      allowNull:true,
+
+    },
+    price:{
+      type:DataTypes.DECIMAL(10,2),
+      allowNull:true,
+
+    },
+    stock:{
+      type:DataTypes.INTEGER,
+      allowNull:true,
+      defaultValue:10
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+      allowNull:true,
+      references:{model:"category",key:"id"},
+    }
   },
   {
     sequelize,
